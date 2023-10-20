ef80693c5f771984b1ce6e6ba2ab987426cd65f8
Victoriajeruto <victoriajeruto7@gmail.com>
2023-10-19 19:01:17 -0700

    a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,7 +10,7 @@ int main(void)
 	p = &n;
 /*
  * write your line of code here...
- * Remember:
+ * remember:a[2] = 98
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
