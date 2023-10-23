4a6d51256dfe547e491bcd220daf29d28da2fe11
Victoriajeruto <victoriajeruto7@gmail.com>
2023-10-23 11:55:39 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _strspn - main function
+  *
+  * @s: Function parameter
+  *
+  * @accept: Function parameter
+  *
+  * Return: Always 0.
+  */
+unsigned int _strspn(char *s, char *accept)
+{
