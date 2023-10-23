d5001652b6fa44a5608a6e86ee89df60c72e93de
Victoriajeruto <victoriajeruto7@gmail.com>
2023-10-23 11:57:39 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,3 +11,24 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
+
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int i = 0;
+	int k;
+
+	while (*s)
+	{
+	for (k = 0; accept[k]; k++)
+	{
+	if (*s == accept[k])
+	{
+	i++;
+	break;
+	}
+	else if (accept[k + 1] == '\0')
+	return (i);
+	}
+	s++;
+	}
+	return (i);
+}
