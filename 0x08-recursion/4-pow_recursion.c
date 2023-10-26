3c5ff11c513b52bf339255b00e2dde252d1e9421
Victoriajeruto <victoriajeruto7@gmail.com>
2023-10-26 09:29:57 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-  * -pow_recursion - function name
+  * _pow_recursion - function name
   * @x: parameter 1
   * @y: parameter 2
   * Return: value of a given number passed to the function
   */
 
 
-int -pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 
 if (y < 0)
@@ -17,7 +17,7 @@ else if (y == 0)
 	return (1);
 
 else
-	return (x * -pow_recursion(x, (y - 1)));
+	return (x * _pow_recursion(x, (y - 1)));
 
 return (0);
 }
